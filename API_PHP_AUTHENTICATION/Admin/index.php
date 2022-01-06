bf8ccbb33cdf9201b2cc5eed6b4ab4350750aba1
Andre Luis <andreluis2608@gmail.com>
2022-01-06 08:27:15 -0300

    inclusao de arquivos para construção do backoffice de gestao de usuarios

---
+++ API_PHP_AUTHENTICATION/Admin/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once('inc/config.php');
+require_once('inc/database.php');
+
+require_once('inc/html_header.php');
+
+echo '<h3> Ola mundo </h3>';
+
+require_once('inc/html_footer.php');
