6710070a234443dc2454196b167bc14f1e8ac91a
Andre Luis <andreluis2608@gmail.com>
2022-01-07 10:30:19 -0300

    mudança de senha do usuario do banco

--- API_PHP/API/inc/config.php
+++ API_PHP/API/inc/config.php
@@ -4,4 +4,4 @@ define('DB_SERVER','localhost');
 define('DB_NAME','loja_api');
 define('DB_CHARSET','utf8');
 define('DB_USERNAME','user_loja_api');
-define('DB_PASSWORD','ga4orez6b61awOfOmE2atoMeKI4OME');
+define('DB_PASSWORD','4Ow3H5wOw717');
